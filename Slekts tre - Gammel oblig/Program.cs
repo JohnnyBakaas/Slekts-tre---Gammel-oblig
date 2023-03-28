ba34d198fb5a7c096bfb72ec49e7fe9e9be86842
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-03-28 06:41:53 +0200

    Add project files.

---
+++ Slekts tre - Gammel oblig/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Slekts_tre___Gammel_oblig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
